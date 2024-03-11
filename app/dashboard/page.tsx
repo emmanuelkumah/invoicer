cdfa7d6566409e85f17cda009856ada9d7277958
Emmanuel Kumah <mailkumah@gmail.com>
2024-03-11 23:27:51 +0000

    Feat: complete form structure

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,158 +10,94 @@ const AppPage = () => {
     console.log("adding new item");
   };
   return (
-    <form className="bg-white p-5 w-full">
-      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-        <div className="flex flex-row gap-4">
-          <label htmlFor="invoiceNumber">Invoice number</label>
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-            <input
-              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900  focus:ring-0 sm:text-sm sm:leading-6"
-              type="number"
-              name=""
-              id="invoiceNumber"
-              min="1"
-              step="1"
-              value={invoiceNumber}
-              //   onChange={(e) => setInvoiceNumber(e.target.value)}
-            />
+    <form className="bg-white p-5 w-full rounded-lg">
+      <section>
+        <h2 className="text-2xl text-center my-3">Invoice Details</h2>
+        <div className="grid grid-cols-1 border-3 gap-4 sm:grid-cols-4">
+          <div>
+            <input type="file" name="" id="" />
           </div>
-        </div>
-        <div className="flex flex-col">
-          <label className="font-bold" htmlFor="from">
-            From{" "}
-          </label>
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-            <input
-              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              type="text"
-              name="from"
-              id="invoiceNumber"
-              placeholder="Who is this invoice from"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <label className="font-bold" htmlFor="bill">
-            Bill to
-          </label>
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-            <input
-              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              type="text"
-              name="bill"
-              id="bill"
-              placeholder="Who is this invoice to?"
-            />
-          </div>
-        </div>
-        <section className="flex flex-col">
           <div className="flex flex-col">
-            <label className="font-bold" htmlFor="bill">
-              Payment terms{" "}
+            <label htmlFor="invoiceNo">Invoice Number</label>
+            <div className="flex relative rounded-lg shadow-sm ring-1 ring-inset ring-gray-300">
+              <span className="flex items-center bg-slate-500 text-cyan-50 border-0 px-2 rounded-l-lg">
+                #
+              </span>
+              <input
+                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900  focus:ring-0 sm:text-sm sm:leading-6"
+                type="number"
+                name="invoiceNumber"
+                id="invoiceNumber"
+                min="1"
+                step="1"
+                value={invoiceNumber}
+                onChange={() => console.log("change nu")}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="invDate">Invoice Date</label>
+            <div className="flex relative rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 sm:max-w-md">
+              <input
+                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900  focus:ring-0 sm:text-sm sm:leading-6"
+                type="date"
+                name="invDate"
+                id="invDate"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="dueDate">Due Date</label>
+            <div className="flex relative rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 sm:max-w-md">
+              <input
+                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900  focus:ring-0 sm:text-sm sm:leading-6"
+                type="date"
+                name="dueDate"
+                id="dueDate"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <h2 className="text-2xl text-center my-3">Business Information</h2>
+        <div className="grid grid-cols-1 border-3 gap-4 sm:grid-cols-2">
+          <div className="flex flex-col">
+            <label className="font-bold py-2" htmlFor="serviceProvider">
+              Service Provider
             </label>
-            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            <div className="flex relative rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 sm:max-w-md">
               <input
                 className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                 type="text"
-                name="bill"
-                id="bill"
-                placeholder="Payment terms"
+                name="serviceProvider"
+                id="serviceProvider"
+                placeholder="Who is this invoice from ?"
               />
             </div>
           </div>
           <div className="flex flex-col">
-            <label className="font-bold" htmlFor="bill">
-              Due Date
+            <label className="font-bold py-2" htmlFor="client">
+              Client
             </label>
-            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            <div className="flex relative rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 sm:max-w-md">
               <input
                 className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                type="date"
-                name="bill"
-                id="bill"
+                type="text"
+                name="client"
+                id="client"
+                placeholder="Who is this invoice to?"
               />
             </div>
-          </div>
-        </section>
-        <section className="flex flex-col border px-3">
-          <div className="flex flex-col">
-            <h4 className="text-4xl ">Amount : $500</h4>
-            <div className="flex gap-3 w-[50%]">
-              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  type="number"
-                  name=""
-                  id=""
-                  size={50}
-                />
-              </div>
-
-              <span>X</span>
-              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  type="number"
-                  size={10}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-            <input
-              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              type="text"
-              name="bill"
-              id="bill"
-              placeholder="Description of product or service"
-            />
-          </div>
-          <button
-            className="w-1/2 rounded-lg bg-slate-700 text-yellow-50 mt-5 py-3"
-            onClick={addNewItem}
-          >
-            Add new item
-          </button>
-        </section>
-        <section className="flex flex-col gap-4">
-          <div className="flex flex-row gap-3">
-            <label htmlFor="">Tax rate</label>
-            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <input
-                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                type="number"
-                name=""
-                id=""
-              />
-            </div>
-          </div>
-          <div className="flex flex-row gap-3">
-            <label htmlFor="">Discount rate</label>
-            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <input
-                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                type="number"
-                name=""
-                id=""
-              />
-            </div>
-          </div>
-        </section>
-        <section>
-          <div>
-            <h3>Subtotal </h3>
-            <span>Total Value </span>
-          </div>
-          <div>
-            <h3>Discount </h3>
-            <span>Total Value </span>
-          </div>
-          <h2>
-            Total : <span>Total amount</span>
-          </h2>
-        </section>
-      </div>
+          </div>{" "}
+        </div>
+      </section>
+      <section>Item Details</section>
+      <section>Sub Totals</section>
+      <section>
+        <button>Preview</button>
+        <button>Generate </button>
+      </section>
     </form>
   );
 };
